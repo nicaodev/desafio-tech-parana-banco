72f7fe391cba226bd4a9e9d39b4949bff0880fef
nicaodev <alexandre.nicos@gmail.com>
2023-07-18 21:33:11 -0300

    repository ok

--- registroClientes.Domain/Interfaces/IRegistroRepository.cs
+++ registroClientes.Domain/Interfaces/IRegistroRepository.cs
@@ -1,5 +1,19 @@
-﻿namespace registroClientes.Domain.Interfaces;
+﻿using registroClientes.Domain.Model;
+
+namespace registroClientes.Domain.Interfaces;
 
 public interface IRegistroRepository
 {
+    Task<Cliente> CadastrarClienteAsync(Cliente cliente);
+
+    Task<IEnumerable<Cliente>> BuscarTodosClientesAsync();
+
+    Task<Cliente> BuscarPorNumeroContatoAsync(string numeroContato);
+
+    Task<Cliente> AtualizarEmailAsync(string email);
+
+    Task<Cliente> AtualizarNumeroContatoAsync(Cliente cliente);
+
+    Task<bool> DeletarPorEmailAsync(string email);
 }
+
--- registroClientes.Infra.Data/Repository/RegistroRepository.cs
+++ registroClientes.Infra.Data/Repository/RegistroRepository.cs
@@ -1,7 +1,82 @@
-﻿using registroClientes.Domain.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using registroClientes.Domain.Interfaces;
+using registroClientes.Domain.Model;
+using registroClientes.Infra.Data.Context;
 
 namespace registroClientes.Infra.Data.Repository;
 
-public class RegistroRepository: IRegistroRepository
+public class RegistroRepository : IRegistroRepository
 {
+    private readonly ApplicationDbContext _context;
+
+    public RegistroRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Cliente> AtualizarEmailAsync(string email)
+    {
+        Cliente cliente = await _context.Clientes.FirstOrDefaultAsync(c => c.Email == email);
+
+        if (cliente is null)
+            return new Cliente();
+
+        cliente.Email = email;
+        await _context.SaveChangesAsync();
+        return cliente;
+    }
+
+    public async Task<Cliente> AtualizarNumeroContatoAsync(Cliente cliente)
+    {
+        Cliente existeCliente = await _context.Clientes.Include(t => t.Telefones).FirstOrDefaultAsync(c => c.Id == cliente.Id);
+
+        if (existeCliente is null) return new Cliente();
+
+        var existeContato = existeCliente.Telefones.FirstOrDefault(t => t.Id == cliente.Id);
+
+        if (existeContato is not null)
+        {
+            cliente.Telefones.ForEach(t =>
+            {
+                existeContato.DDD_Numero = t.DDD_Numero;
+            });
+        }
+
+        await _context.SaveChangesAsync();
+        return existeCliente;
+    }
+
+    public async Task<Cliente> BuscarPorNumeroContatoAsync(string numeroContato)
+    {
+        Cliente cliente = await _context.Clientes
+            .Include(c => c.Telefones)
+            .FirstOrDefaultAsync(c => c.Telefones.Any(p => p.DDD_Numero == numeroContato)) ?? new Cliente();
+
+        return cliente;
+    }
+
+    public async Task<IEnumerable<Cliente>> BuscarTodosClientesAsync()
+    {
+        return await _context.Clientes.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Cliente> CadastrarClienteAsync(Cliente cliente)
+    {
+        _context.Clientes.Add(cliente);
+        await _context.SaveChangesAsync();
+        return cliente;
+    }
+
+    public async Task<bool> DeletarPorEmailAsync(string email)
+    {
+        Cliente cliente = await _context.Clientes.FirstOrDefaultAsync(c => c.Email == email);
+
+        if (cliente is null)
+            return false;
+
+        _context.Clientes.Remove(cliente);
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
 }
